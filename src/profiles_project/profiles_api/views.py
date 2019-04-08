a1d57defacdec8a70ac78b16db6223076cff9d56
prakash <prks280@gmail.com>
2019-04-08 23:18:52 +0530

    first api view

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloAPIView(APIView):
+    """Test API"""
+
+    def get(self, request, format=None):
+        """Returns a list of API features"""
+        an_apiview = ['cp','aliya','none','nana']
+        return Response({'message':'hello', 'an_apiview':an_apiview})
