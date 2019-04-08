441390001941e401461b097cba8240422383176b
prakash <prks280@gmail.com>
2019-04-09 00:00:39 +0530

    added get post patch delete

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from . import serializers
+from rest_framework import status
 
 
 class HelloAPIView(APIView):
-    """Test API"""
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
-        """Returns a list of API features"""
         an_apiview = ['cp','aliya','none','nana']
         return Response({'message':'hello', 'an_apiview':an_apiview})
+
+    def post(self, request):
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name=serializer.data.get('name')
+            message = 'hello {}'.format(name)
+            return Response({'message':message})
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        return Response({'method':'put'})
+
+    def patch(self,request, pk=None):
+        return Response({'method':'patch'})
+
+
+    def delete(self,request,pk=None):
+        return Response({'method':'delete'})
