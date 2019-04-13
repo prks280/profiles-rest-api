483de60950c7581def8f6c3f218ea7459fd5d947
prakash <prks280@gmail.com>
2019-04-13 19:06:40 +0530

    added login api

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,16 +1,17 @@
-from rest_framework.authtoken.serializers import AuthTokenSerializer
-from rest_framework.authtoken.views import ObtainAuthToken
-# Create your views here.
-
 from django.shortcuts import render
 
 from rest_framework import viewsets
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.authentication import TokenAuthentication
+from rest_framework import filters
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.authtoken.views import ObtainAuthToken
 
 from . import serializers
 from . import models
+from . import permissions
 
 # Create your views here.
 
@@ -115,28 +116,18 @@ class UserProfileViewSet(viewsets.ModelViewSet):
 
     serializer_class = serializers.UserProfileSerializer
     queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('name', 'email',)
 
 
 class LoginViewSet(viewsets.ViewSet):
-    """checks email and password and rerturns the token"""
+    """Checks email and password and returns an auth token."""
+
     serializer_class = AuthTokenSerializer
-    def create (self,request):
-        """use the ObtainAuthToekn API view to validate and create a token"""
-        return ObtainAuthToken(request)
 
+    def create(self, request):
+        """Use the ObtainAuthToken APIView to validate and create a token."""
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        return ObtainAuthToken().post(request)
